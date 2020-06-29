9d434464978fecd900f5415ca8a6535017837b9b
Savant_Cat <scanf@me.com>
2020-06-29 19:03:53 +0900

    サンプル修正

--- examples/angleDaisyChain/angleDaisyChain.ino
+++ examples/angleDaisyChain/angleDaisyChain.ino
@@ -8,8 +8,13 @@ L6470Stepper stepper(pinCS, CHAINED_MOTOR_NUM);	// pass CS pin number & number o
 
 void setup()
 {
+  Serial.begin(9600);
+  
 	stepper.setup();
-	stepper.angle(0);	// this makes all motors do the same motion
+  stepper.angle(0);
+  stepper.resetPos();
+  stepper.softStop();
+	//stepper.angle(0);	// this makes all motors do the same motion
 }
 
 void loop()
@@ -19,12 +24,20 @@ void loop()
 	delay(2000);
 
 	if (goForward) {
-		stepper.angle(360);
+		stepper.setAngle(180,0);
+    stepper.setAngle(360,1);
 	} else {
-		stepper.angle(0);
+		stepper.setAngle(0,0);
+    stepper.setAngle(0,1);
 	}
+
+  stepper.execute();
+
+  Serial.print(stepper.getCurrAngle(0));
+  Serial.print(",");
+  Serial.println(stepper.getCurrAngle(1));
+  
 
 	// switch direction
 	goForward = !goForward;
 }
-
